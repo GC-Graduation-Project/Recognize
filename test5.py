851ced541494868e86d02c2c88a15422b08f07cf
gyuwon <licmm0520@gmail.com>
2023-12-02 04:09:48 +0900

    detect example 수정

--- test5.py
+++ test5.py
@@ -15,15 +15,15 @@ image_0, subimages = modules.remove_noise(image)
 # normalized_images : 오선 제거된 분할 이미지와 오선 정보에 대해 정규화된 이미지를 가지고있는 리스트
 
 normalized_images, stave_list = modules.digital_preprocessing(image_0,subimages)
-normalized_images[1] = cv2.bitwise_not(normalized_images[1])
-
-result = detect(cv2.cvtColor(normalized_images[1], cv2.COLOR_GRAY2BGR))
+for img in normalized_images:
+    img = cv2.bitwise_not(img)
+    result = detect(cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)) # YOLO모델에는 BGR로 들어가야하기때문에 convert해서 넣어줌.
 
 print(result)
 image_2 = cv2.bitwise_not(image_0)
 
 # 이미지 띄우기
-cv2.imshow('image2', normalized_images[1])
+cv2.imshow('image2', normalized_images[6])
 k = cv2.waitKey(0)
 if k == 27:
     cv2.destroyAllWindows()
