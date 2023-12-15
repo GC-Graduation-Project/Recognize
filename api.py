6d6de22d0cd5a76c69228f9e4e86a229b66c4db1
Gyuwon-Jung <licmm0520@gmail.com>
2023-12-15 17:54:49 +0900

    필요없는파일 삭제

--- api.py
+++ api.py
@@ -1,47 +1,53 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse
 import cv2
 import numpy as np
 import modules as md
 import functions as fs
 
 app = FastAPI()
+
 @app.get("/")
-def HelloWorld():
-    return FastAPI
+def hello_world():
+    return {"message": "Hello World"}
 
 @app.post("/uploadfile")
 async def create_upload_file(file: UploadFile = File(...)):
-    contents = await file.read()
-    nparr = np.frombuffer(contents, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    try:
+        # 파일 내용 읽기
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    final_list = []
+        # 이미지 처리 및 분석
+        final_list = []
+        image_0, subimages = md.remove_noise(img)
+        normalized_images, stave_list = md.digital_preprocessing(image_0, subimages)
+        rec_list, note_list, rest_list = md.beat_extraction(normalized_images)
+        note_list2, pitch_list = md.pitch_extraction(stave_list, normalized_images)
 
-    image_0, subimages = md.remove_noise(img)
+        # 음표 처리 및 기타 탭 변환
+        for i, (rec, pitches) in enumerate(zip(rec_list, pitch_list)):
+            sharps, flats = fs.count_sharps_flats(rec)
+            temp_dict = {}
+            modified_pitches = fs.modify_notes(pitches, sharps, flats)
+            for pit in modified_pitches:
+                positions = fs.get_guitar(pit)
+                temp_dict[pit] = positions
+            modified_pitches = fs.calculate_efficient_positions(modified_pitches, temp_dict)
+            pitch_list[i] = modified_pitches
 
-    normalized_images, stave_list = md.digital_preprocessing(image_0, subimages)
+        for note2, note1 in zip(note_list2, note_list):
+            note2[1:] = fs.update_notes(note2[1:], note1)
 
-    rec_list, note_list, rest_list = md.beat_extraction(normalized_images)
+        for list1, list2, list3 in zip(rec_list, note_list2, pitch_list):
+            m_list = fs.merge_three_lists(list1, list2, list3)
+            final_list.append(m_list)
 
-    note_list2, pitch_list = md.pitch_extraction(stave_list, normalized_images)
+        # 결과 문자열 생성
+        sen = fs.convert_to_sentence(final_list)
 
-    for rec, pitches in zip(rec_list, pitch_list):
-        sharps, flats = fs.count_sharps_flats(rec)
-        pitches = fs.modify_notes(pitches, sharps, flats)
-
-    for note2, note1 in zip(note_list2, note_list):
-        note2[1:] = fs.update_notes(note2[1:], note1)
-
-    for list1, list2, list3 in zip(rec_list, note_list2, pitch_list):
-        m_list = fs.merge_three_lists(list1, list2, list3)
-        final_list.append(m_list)
-
-    print(final_list)
-
-    sen = fs.convert_to_sentence(final_list)
-
-    print(sen)
-
-    # 결과를 JSON 형식으로 반환
-    return JSONResponse(content={"results": sen})
+        # 텍스트 형식으로 반환
+        return PlainTextResponse(content=sen)
+    except Exception as e:
+        return PlainTextResponse(content=str(e), status_code=500)
--- apitest.py
+++ apitest.py
@@ -1,17 +1,13 @@
 import requests as r
-import json
-from pprint import pprint
 
-
-def send_request(image='resources/music1.jpg'):
-    files = {'file': ('image_filename.png', open(image, 'rb'))}
+def send_request(image='resources/music.jpg'):
+    files = {'file': ('image_filename.jpg', open(image, 'rb'))}
     res = r.post("http://localhost:8000/uploadfile/", files=files)
 
     if res.status_code == 200:
         print("Request Successful")
-        pprint(json.loads(res.text))
+        print(res.text)  # 응답을 일반 텍스트로 출력
     else:
         print(f"Request Failed with status code {res.status_code}")
-
 
 send_request()
